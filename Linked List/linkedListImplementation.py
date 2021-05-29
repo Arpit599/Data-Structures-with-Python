cbe34ada8c9d4a204b81fade2b02309295f039dd
Arpit Bharadwaj <arpit.bharadwaj5@gmail.com>
2021-05-29 11:50:33 +0530

    Node insertion implemented

--- Linked List/linkedListImplementation.py
+++ Linked List/linkedListImplementation.py
@@ -6,6 +6,30 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    
+    def insertAtFront(self, data):
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
+
+    def insertAfter(self, prev, data):
+        if prev is None:
+            print("Please enter a valid previous node")
+        newNode = Node(data)
+        newNode.next = prev.next
+        prev.next = newNode
+
+    def insertAtEnd(self, data):
+        newNode = Node(data)
+
+        if self.head is None:
+            self.head = newNode
+            return
+
+        temp = self.head
+        while(temp.next != None):
+            temp = temp.next
+        temp.next = newNode
 
     def printList(self):
         temp = self.head
@@ -23,4 +47,9 @@ if __name__ == "__main__" :
     llist.head.next = second
     second.next = third
 
+    # llist.insertAtFront(4)
+    # llist.insertAtEnd(5)
+    # llist.insertAtFront(6)
+    # llist.insertAtEnd(7)
+    llist.insertAfter(llist.head.next.next, 4)
     llist.printList()
