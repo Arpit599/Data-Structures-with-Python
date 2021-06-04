f404d283ab494e0f5a6216c0d8e82ee3c09da6aa
Arpit Bharadwaj <arpit.bharadwaj5@gmail.com>
2021-06-04 16:38:26 +0530

    Node deletion code added

--- Linked List/linkedList.py
+++ Linked List/linkedList.py
@@ -52,13 +52,62 @@ class LinkedList:
 
         if self.head is None:
             self.head = newNode
+        else:
+            temp = self.head
+            while(temp.next != None):
+                temp = temp.next
+            temp.next = newNode
+        self.nodeCount += 1
+
+    def deleteHead(self):
+        if self.head == None:
+            print("Can't delete node from empty list")
             return
 
+        self.head = self.head.next
+        self.nodeCount -= 1
+    
+    def deleteAfter(self, nodeNum):
+        if nodeNum >= self.nodeCount:
+            print("Not enough nodes!")
+            return
+        
         temp = self.head
-        while(temp.next != None):
+        for i in range(nodeNum - 1):
             temp = temp.next
-        temp.next = newNode
-        self.nodeCount += 1
+        temp.next = temp.next.next
+        self.nodeCount -= 1
+
+    def deleteBefore(self, nodeNum):
+        if nodeNum == 1:
+            print("Invalid operation!...No nodes before head to delete")
+            return
+        elif nodeNum > self.nodeCount:
+            print("Not enough nodes!")
+            return
+        elif nodeNum == 2:
+            self.head = self.head.next
+        else:
+            temp = self.head
+            for i in range(nodeNum - 3):
+                temp = temp.next
+            temp.next = temp.next.next
+        self.nodeCount -= 1
+    
+    def deleteEnd(self):
+        if self.head == None:
+            print("Can't delete node from empty list")
+            return 
+
+        if self.head.next == None:
+            self.head = None
+        else:
+            temp = self.head 
+            while(temp.next.next != None):
+                temp = temp.next
+            temp.next = None
+
+        self.nodeCount -= 1
 
     def printList(self):
         temp = self.head
@@ -76,8 +125,12 @@ if __name__ == "__main__" :
         print("2. Insert node after a node")
         print("3. Insert node before a node")
         print("4. Insert node at the end")
-        print("5. Print list")
-        print("6. Print number of nodes")
+        print("5. Delete node at the front")
+        print("6. Delete node after a node")
+        print("7. Delete node before a node")
+        print("8. Delete node at the end")
+        print("9. Print list")
+        print("10. Print number of nodes")
 
         choice = int(input("Enter your choice: "))
         if(choice == 1):
@@ -96,8 +149,18 @@ if __name__ == "__main__" :
             data = int(input("Enter the node value: "))
             llist.insertAtEnd(data)
         elif(choice == 5):
-            llist.printList()
+            llist.deleteHead()
         elif(choice == 6):
+            nodeNumber = int(input("Enter the number of node after which you want to delete: "))
+            llist.deleteAfter(nodeNumber)
+        elif(choice == 7):
+            nodeNumber = int(input("Enter the number of node after which you want to delete: "))
+            llist.deleteBefore(nodeNumber)
+        elif(choice == 8):
+            llist.deleteEnd()
+        elif(choice == 9):
+            llist.printList()
+        elif(choice == 10):
             print("Number of nodes:", llist.nodeCount)
         else: 
             print("Wrong choice")
