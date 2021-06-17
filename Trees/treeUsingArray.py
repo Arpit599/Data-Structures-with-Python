c8fe2677315368ee8c98846c84148fe50256d864
Arpit Bharadwaj <arpit.bharadwaj5@gmail.com>
2021-06-17 23:45:40 +0530

    Minor updates

--- Trees/treeUsingArray.py
+++ Trees/treeUsingArray.py
@@ -1,4 +1,24 @@
-tree = [None] * 10
+class Tree:
+    def __init__(self):
+        self.tree = []
 
-def insertion(nodeValue):
+    def insertion(self, nodeValue):
+        self.tree.append(nodeValue)
+        self.tree.extend(["-", "-"])
+        
+    
+    def deleteion(self, nodeValue):
+        pass
+
+    def search(self, nodeValue):
+        pass
+
+    def inOrderTraversal(self):
+        pass
+
+    def preOrderTraversal(self):
+        pass
+
+    def postOrderTraversal(self):
+        pass
     
